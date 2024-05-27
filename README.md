e641e02d02ba7370383e93686bd08b86affaee6c
norsu <mohammedameen9011@gmail.com>
2024-05-28 03:24:41 +0530

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # corrective-rag-llama3
+
+![alt text](image.png)
+
+Corrective RAG  application using LLAMA3 8B
+
+## Installation Instructions
+
+1. Install Ollama and LLAMA3 8B:Instruct.
+
+2. Install dependencies:
+
+    ```bash
+    pip install -r requirements.txt
+    ```
+
+3. Run the webapp:
+
+    ```bash
+    streamlit run app.py
+    ```
--- corrective-rag/graph/graph.py
+++ corrective-rag/graph/graph.py
@@ -4,7 +4,6 @@ from langgraph.graph import StateGraph, END
 from .nodes import (
     retrieve,
     grade_documents,
-    transform_query,
     web_search_node,
     generate,
     decide_to_generate,
@@ -23,18 +22,18 @@ def corrective_rag():
     workflow = StateGraph(GraphState)
     workflow.add_node("retrieve", retrieve)
     workflow.add_node("grade_documents", grade_documents)
-    workflow.add_node("transform_query", transform_query)
+    # workflow.add_node("transform_query", transform_query)
     workflow.add_node("web_search_node", web_search_node)
     workflow.add_node("generate", generate)
 
-    workflow.set_entry_point("transform_query")
-    workflow.add_edge("transform_query", "retrieve")
+    workflow.set_entry_point("retrieve")
     workflow.add_edge("retrieve", "grade_documents")
     workflow.add_conditional_edges(
         "grade_documents",
         decide_to_generate,
-        {"generate": "generate", "web_search_node": "web_search_node"},
+        {"generate": "generate", "web_search_nodde": "web_search_node"},
     )
+    # workflow.add_edge("transform_query", "web_search_node")
     workflow.add_edge("web_search_node", "generate")
     workflow.add_edge("generate", END)
 
